553bc0ac418597aecc7516d2884f1575bce9cdc5
Andrew McWatters <me@andrewmcwatters.com>
2011-07-29 22:07:37 +0000

    Fixed data access issues with color Fixed NULL checks with vectors and angles Adding Barrel Deathmatch placeholder code

---
+++ gamemodes/barreldeathmatch/gamemode/shared.lua
@@ -0,0 +1,10 @@
+--========== Copyleft © 2010, Team Sandbox, Some rights reserved. ===========--
+--
+-- Purpose:
+--
+--===========================================================================--
+
+GM.Name			= "Barrel Deathmatch"
+GM.Homepage		= "http://www.half-life2sandbox.com/"
+GM.Developer	= "Team Sandbox"
+GM.Manual		= "http://www.half-life2sandbox.com/manual"
