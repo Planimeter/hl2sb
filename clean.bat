f6a707d588a10ff959918de3205da2e90ebf1ff2
Andrew McWatters <me@andrewmcwatters.com>
2011-09-04 18:25:21 +0000

    clean.bat now purges the SAVE folder

--- clean.bat
+++ clean.bat
@@ -1,3 +1,3 @@
-rd "cache" "DownloadLists" "downloads" "maps\graphs" "maps\soundcache" "materials\VGUI" "materialsrc" /s /q
+rd "cache" "DownloadLists" "downloads" "maps\graphs" "maps\soundcache" "materials\VGUI" "materialsrc" "SAVE" /s /q
 del "bin\client.pdb" "bin\Server.pdb" "cfg\config.cfg" "demoheader.tmp" "modelsounds.cache" "serverconfig.vdf" "stats.txt" "textwindow_temp.html" "voice_ban.dt" /f /s /q
 pause
