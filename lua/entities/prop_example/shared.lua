32d8de78aa7063a012942f7e3af2db3e403ae9b0
Andrew McWatters <me@andrewmcwatters.com>
2011-09-28 23:22:13 +0000

    Changed the execution chain of the VPhysicsUpdate hook Fixed prediction issues with prop_example based on shared initialization code

--- lua/entities/prop_example/shared.lua
+++ lua/entities/prop_example/shared.lua
@@ -4,23 +4,6 @@
 --
 --===========================================================================--
 
-local Warning = dbg.Warning
-
-function ENT:Initialize()
-	local allowPrecache = self.IsPrecacheAllowed();
-	self.SetAllowPrecache( true );
-	self.PrecacheModel( "models/props_junk/wood_crate001a.mdl" );
-	self:SetModel( "models/props_junk/wood_crate001a.mdl" )
-	self.SetAllowPrecache( allowPrecache );
-	local pPhysicsObject = self:VPhysicsInitNormal( 6, 0, false );
-
-	if ( not pPhysicsObject ) then
-		self:SetSolid( 0 );
-		self:SetMoveType( 0 );
-		Warning("ERROR!: Can't create physics object for " .. self:GetModelName() .. "\n" );
-	end
-end
-
 local velocity, angularVelocity;
 
 function ENT:VPhysicsUpdate( pPhysics )
