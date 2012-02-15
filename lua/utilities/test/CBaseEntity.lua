5d382d3991a0a13e4bad5d270a2abdb7a3504dde
Andrew McWatters <me@andrewmcwatters.com>
2012-02-15 19:04:27 +0000

    Improved type checking in small places internally Added CBaseEntity test case Updated FileHandle_t test case

---
+++ lua/utilities/test/CBaseEntity.lua
@@ -0,0 +1,8 @@
+--======== Copyleft © 2010-2011, Team Sandbox, Some rights reserved. ========--
+--
+-- Purpose: Tests the implementation of CBaseEntity.
+--
+--===========================================================================--
+
+print( "Testing error handling... (Should return error regarding indexing a NULL entity!)" )
+print( NULL.m_nIndex )
--- lua/utilities/test/FileHandle_t.lua
+++ lua/utilities/test/FileHandle_t.lua
@@ -4,7 +4,10 @@
 --
 --===========================================================================--
 
+require( "dbg" )
 require( "filesystem" )
+
+local Warning = dbg.Warning
 
 local hFile = filesystem.Open( "gamecontent.txt", "r" )
 local size = filesystem.Size( hFile )
@@ -16,20 +19,24 @@ print( "Closing hFile, handle should become invalid!" )
 filesystem.Close( hFile )
 print( "hFile = " .. tostring( hFile ) )
 
--- Andrew; this will error intentionally as a part of the test to ensure
--- luaL_checkfilehandle() works internally.
 print( "Testing error handling..." )
--- While this may seem confusing at first, filesystem.IsOk() doesn't seem to
--- allow FILESYSTEM_INVALID_HANDLE, despite it's name indicating checking
--- validity of any kind. It will error at the engine level and print a warning,
--- but not crash the game. As a result, it's valid for testing error handling
--- here to reflect actual Source SDK usage, even though we sort of baby
--- developers here at our auxiliary library level.
-print( "filesystem.IsOk( hFile ) = " .. tostring( filesystem.IsOk( hFile ) ) )
+local status, strError = pcall( function()
+	print( "filesystem.IsOk( hFile ) = " .. tostring( filesystem.IsOk( hFile ) ) )
+end )
 
-print( "Testing error handling with non-filehandle userdatum...")
--- Internally, this function doesn't use luaL_checkfilehandle(), but rather
--- lua_tofilehandle(). Here we test converting non-userdata to plain userdatum
--- to FileHandle_t internally. Crashing at this level will indicate us using an
--- invalid reference.
-print( "filesystem.IsOk( hFile ) = " .. tostring( filesystem.IsOk( "ABC" ) ) )
+if ( status == false ) then
+	Warning( strError .. "\n" )
+else
+	error( "WARNING: test did not fail as expected on line 32!", 2 )
+end
+
+print( "Testing error handling with non-filehandle type...")
+local status, strError = pcall( function()
+	print( "filesystem.IsOk( hFile ) = " .. tostring( filesystem.IsOk( -1 ) ) )
+end )
+
+if ( status == false ) then
+	Warning( strError .. "\n" )
+else
+	error( "WARNING: test did not fail as expected on line 47!", 2 )
+end
