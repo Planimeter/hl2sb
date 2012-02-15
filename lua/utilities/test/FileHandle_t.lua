77fd0699a92dba63f45695ade91fe9a0452302bd
Andrew McWatters <me@andrewmcwatters.com>
2012-02-15 17:51:23 +0000

    Fixed multiple improper error messages thrown by various auxiliary Source Engine Lua API functions Modified FileHandle_t.lua test case

--- lua/utilities/test/FileHandle_t.lua
+++ lua/utilities/test/FileHandle_t.lua
@@ -7,8 +7,29 @@
 require( "filesystem" )
 
 local hFile = filesystem.Open( "gamecontent.txt", "r" )
-print( hFile )
-local size, pOutput = filesystem.Read( hFile )
-print( "size: " .. size )
+local size = filesystem.Size( hFile )
+print( "hFile = " .. tostring( hFile ) )
+local bytesRead, pOutput = filesystem.Read( size, hFile )
+print( "bytes: " .. bytesRead )
 print( "output:\n" .. pOutput )
+print( "Closing hFile, handle should become invalid!" )
 filesystem.Close( hFile )
+print( "hFile = " .. tostring( hFile ) )
+
+-- Andrew; this will error intentionally as a part of the test to ensure
+-- luaL_checkfilehandle() works internally.
+print( "Testing error handling..." )
+-- While this may seem confusing at first, filesystem.IsOk() doesn't seem to
+-- allow FILESYSTEM_INVALID_HANDLE, despite it's name indicating checking
+-- validity of any kind. It will error at the engine level and print a warning,
+-- but not crash the game. As a result, it's valid for testing error handling
+-- here to reflect actual Source SDK usage, even though we sort of baby
+-- developers here at our auxiliary library level.
+print( "filesystem.IsOk( hFile ) = " .. tostring( filesystem.IsOk( hFile ) ) )
+
+print( "Testing error handling with non-filehandle userdatum...")
+-- Internally, this function doesn't use luaL_checkfilehandle(), but rather
+-- lua_tofilehandle(). Here we test converting non-userdata to plain userdatum
+-- to FileHandle_t internally. Crashing at this level will indicate us using an
+-- invalid reference.
+print( "filesystem.IsOk( hFile ) = " .. tostring( filesystem.IsOk( "ABC" ) ) )
