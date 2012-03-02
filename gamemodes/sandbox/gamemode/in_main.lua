ba9bee3015fcef59b695dc2ca708b6288fcce887
Andrew McWatters <me@andrewmcwatters.com>
2012-03-02 16:36:33 +0000

    Added _R.CBasePlayer.GetLocalPlayer()

--- gamemodes/sandbox/gamemode/in_main.lua
+++ gamemodes/sandbox/gamemode/in_main.lua
@@ -9,27 +9,23 @@ local FCVAR_CLIENTDLL = _E.FCVAR.CLIENTDLL
 require( "concommand" )
 
 local function IN_BuildDown( pPlayer, pCmd, args )
-if not _CLIENT then
-	if ( pPlayer == NULL ) then
+	if ( pPlayer ~= NULL ) then
 		local pWeapon = pPlayer:GetActiveWeapon()
 		if ( pWeapon ~= NULL ) then
 			pWeapon:Lower()
 		end
 	end
 end
-end
 
 concommand.Create( "+buildmenu", IN_BuildDown, nil, FCVAR_CLIENTDLL )
 
 local function IN_BuildUp( pPlayer, pCmd, args )
-if not _CLIENT then
-	if ( pPlayer == NULL ) then
+	if ( pPlayer ~= NULL ) then
 		local pWeapon = pPlayer:GetActiveWeapon()
 		if ( pWeapon ~= NULL ) then
-			pWeapon:Raise()
+			pWeapon:Ready()
 		end
 	end
-end
 end
 
 concommand.Create( "-buildmenu", IN_BuildUp, nil, FCVAR_CLIENTDLL )
