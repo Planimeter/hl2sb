d4cd8a593567944145a88725a9e10b81aaa2d4b8
Andrew McWatters <me@andrewmcwatters.com>
2012-03-12 02:39:04 +0000

    Removed old and obsolete test cases

--- lua/utilities/test/CBaseEntity.lua
+++
@@ -1,8 +0,0 @@
---======== Copyleft © 2010-2011, Team Sandbox, Some rights reserved. ========--
---
--- Purpose: Tests the implementation of CBaseEntity.
---
---===========================================================================--
-
-print( "Testing error handling... (Should return error regarding indexing a NULL entity!)" )
-print( NULL.m_nIndex )
