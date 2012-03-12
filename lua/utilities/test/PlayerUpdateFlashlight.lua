c8f0e6016ed60788951c50005804323cec98b148
Andrew McWatters <me@andrewmcwatters.com>
2012-03-12 05:14:57 +0000

    Fixed not committing updated binaries Updated PlayerUpdateFlashlight.lua

--- lua/utilities/test/PlayerUpdateFlashlight.lua
+++ lua/utilities/test/PlayerUpdateFlashlight.lua
@@ -4,5 +4,24 @@
 --
 --===========================================================================--
 
-hook.add( "PlayerUpdateFlashlight", "PlayerUpdateWeaponFlashlight", function( pPlayer, position, vecForward, vecRight, vecUp, nDistance )
+hook.add( "PlayerUpdateFlashlight", "PlayerUpdateWeaponFlashlight", function( pHL2MPPlayer, position, vecForward, vecRight, vecUp, nDistance )
+	--if ( not input.CAM_IsThirdPerson() ) then
+		print( pHL2MPPlayer )
+		local iAttachment = pHL2MPPlayer:LookupAttachment( "anim_attachment_RH" );
+
+		if ( iAttachment >= 0 ) then
+			local vecOrigin = Vector();
+			--Tony; EyeAngles will return proper whether it's local player or not.
+			local eyeAngles = pHL2MPPlayer:EyeAngles();
+
+			pHL2MPPlayer:GetAttachment( iAttachment, vecOrigin, eyeAngles );
+
+			local vForward = Vector();
+			AngleVectors( eyeAngles, vecForward, vecRight, vecUp );
+			position = vecOrigin;
+		else
+			pHL2MPPlayer:EyeVectors( vecForward, vecRight, vecUp );
+		end
+		return position, vecForward, vecRight, vecUp, nDistance
+	--end
 end )
