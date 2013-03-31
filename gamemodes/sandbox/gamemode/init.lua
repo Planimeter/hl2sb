1bd3c544c8dcdfd2eb36ab17dbc40cbc7f8e0a66
Andrew McWatters <me@andrewmcwatters.com>
2013-03-31 23:20:06 +0000

    Added new spawnpoint selection code to Sandbox gamemode

--- gamemodes/sandbox/gamemode/init.lua
+++ gamemodes/sandbox/gamemode/init.lua
@@ -6,17 +6,41 @@
 
 include( "shared.lua" )
 
+local tSpawnPointClassnames = {
+  "info_player_deathmatch",
+  "info_player_combine",
+  "info_player_rebel",
+  "info_player_terrorist",
+  "info_player_counterterrorist",
+  "info_player_axis",
+  "info_player_allies",
+  "info_player_start"
+}
+
 function GM:FlItemRespawnTime( pItem )
   return -1
+end
+
+function GM:GiveDefaultItems( pPlayer )
+  pPlayer:GiveNamedItem( "weapon_physgun" )
+  self.BaseClass:GiveDefaultItems( pPlayer )
 end
 
 function GM:ItemShouldRespawn( pItem )
   return 6 -- GR_ITEM_RESPAWN_NO
 end
 
-function GM:GiveDefaultItems( pPlayer )
-  pPlayer:GiveNamedItem( "weapon_physgun" )
-  self.BaseClass:GiveDefaultItems( pPlayer )
+function GM:PlayerEntSelectSpawnPoint( pHL2MPPlayer )
+  local tSpawnPoints = {}
+  local pSpot = NULL
+  for _, classname in ipairs( tSpawnPointClassnames ) do
+    pSpot = gEntList.FindEntityByClassname( NULL, classname )
+    while ( pSpot ~= NULL ) do
+      table.insert( tSpawnPoints, pSpot )
+      pSpot = gEntList.FindEntityByClassname( pSpot, classname )
+    end
+  end
+  return tSpawnPoints[ math.random( 1, #tSpawnPoints ) ]
 end
 
 function GM:PlayerPickupObject( pHL2MPPlayer, pObject, bLimitMassAndSize )
