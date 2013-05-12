44221345c9e7c7925ca39f31f35628d5f5368467
Andrew McWatters <me@andrewmcwatters.com>
2013-05-12 00:09:44 +0000

    Updated fix for CBaseCombatWeapon:WeaponSound() Added ignore to materials/temp

--- lua/includes/extensions/weapon.lua
+++ lua/includes/extensions/weapon.lua
@@ -11,17 +11,21 @@ local gpGlobals = gpGlobals
 -- HACKHACK: We override this here, because for some reason scripted weapons
 -- don't properly play weapon sounds in singleplayer
 function _R.CBaseCombatWeapon.WeaponSound( self, sound_type, soundtime )
+  if ( _CLIENT ) then
+    return
+  end
+  
   -- Only the player fires this way so we can cast
   local pPlayer = self:GetOwner();
 
-  if ( gpGlobals.maxClients() > 1 ) then
-    WeaponSound( self, sound_type, soundtime );
-  else
+  -- if ( gpGlobals.maxClients() > 1 ) then
+  --   WeaponSound( self, sound_type, soundtime );
+  -- else
     local shootsound = self:GetShootSound( sound_type );
-    if ( pPlayer ~= NULL ) then
+    if ( ToBaseEntity( pPlayer ) ~= NULL ) then
       pPlayer:EmitSound( shootsound ); 
     else
       self:EmitSound( shootsound ); 
     end
-  end
+  -- end
 end
