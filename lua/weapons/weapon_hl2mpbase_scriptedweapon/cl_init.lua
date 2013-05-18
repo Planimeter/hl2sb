c4168df64af62ae23d7fe0e494cc87b0a9c089ea
Andrew McWatters <me@andrewmcwatters.com>
2013-05-18 08:10:37 +0000

    Filled DrawModel stub in weapon_hl2mpbase_scriptedweapon/cl_init.lua

--- lua/weapons/weapon_hl2mpbase_scriptedweapon/cl_init.lua
+++ lua/weapons/weapon_hl2mpbase_scriptedweapon/cl_init.lua
@@ -9,5 +9,8 @@ include( "shared.lua" )
 function SWEP:DrawLargeWeaponBox( bSelected, xpos, ypos, boxWide, boxTall, selectedColor, alpha, number )
 end
 
+function SWEP:DrawModel( flags )
+end
+
 function SWEP:MuzzleFlash( pos1, angles, type, firstPerson )
 end
